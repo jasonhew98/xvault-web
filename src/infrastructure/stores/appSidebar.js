1aafe9d536f93e79812f03c00c9c911b88555f62
Jason <jasonhew1211@gmail.com>
2024-03-09 18:15:12 +0800

    Fix whitespace

--- src/infrastructure/stores/appSidebar.js
+++ src/infrastructure/stores/appSidebar.js
@@ -3,13 +3,13 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useAppSidebarStore = defineStore('sidebar', () => {
-    const isAppSidebarOpen = ref(false);
+	const isAppSidebarOpen = ref(false);
 	const page = useStorage('page', ref('transaction'));
-    const selected = useStorage('selected', ref('transaction'));
+	const selected = useStorage('selected', ref('transaction'));
 
-    function toggleSidebar() {
-		isAppSidebarOpen.value = !isAppSidebarOpen.value
-    }
+	function toggleSidebar() {
+		isAppSidebarOpen.value = !isAppSidebarOpen.value;
+	}
 
-    return { isAppSidebarOpen, toggleSidebar, selected, page }
+	return { isAppSidebarOpen, toggleSidebar, selected, page }
 })
--- src/infrastructure/stores/darkMode.js
+++ src/infrastructure/stores/darkMode.js
@@ -3,14 +3,14 @@ import { defineStore } from 'pinia'
 import { useStorage } from '@vueuse/core'
 
 export const useDarkModeStore = defineStore('darkMode', () => {
-    const darkMode = useStorage('darkMode', ref(false))
+	const darkMode = useStorage('darkMode', ref(false));
 
-    document.documentElement.classList.toggle('dark', darkMode.value)
+	document.documentElement.classList.toggle('dark', darkMode.value);
 
-    function toggleDarkMode() {
-		darkMode.value = !darkMode.value
-		document.documentElement.classList.toggle('dark', darkMode.value)
-    }
+	function toggleDarkMode() {
+		darkMode.value = !darkMode.value;
+		document.documentElement.classList.toggle('dark', darkMode.value);
+	}
 
-    return { darkMode, toggleDarkMode }
+	return { darkMode, toggleDarkMode }
 })
