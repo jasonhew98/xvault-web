626b1547a8d0555f19b2fd0cab87a2b6b5145e75
Jason <jasonhew1211@gmail.com>
2024-05-03 23:09:25 +0800

    Update code

--- src/features/Application/Wallet/WalletIndexPage.vue
+++ src/features/Application/Wallet/WalletIndexPage.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="w-full h-full overflow-auto flex max-w-full flex-auto flex-col">
-    <h1 class="text-green">Wallet</h1>
+  <div class="w-full h-full overflow-auto flex max-w-full flex-col">
+    <main id="page-content" class="w-full flex max-w-full flex-col">
+      <div class="container w-full p-4">
+      </div>
+    </main>
   </div>
 </template>
 
